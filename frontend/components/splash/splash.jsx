68eba99d95effa51bd0438bbedf452b864266643
pfernandes <phelipe.fern@gmail.com>
2019-10-03 23:10:35 -0400

    Add more images to the splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -24,6 +24,43 @@ const Splash = props => {
                 </div>
                 <div className='jetpack-pug'></div>
             </div>
+            <div className='splash-assets'>
+                <div className='splash-assets-content'>
+                    <div className='stocks-img'></div>
+                    <h1>Stocks</h1>
+                    <p>Invest in companies you love and build out your perfect portfolio.</p>
+                    <p className='splash-links'>Learn about stocks</p>
+                </div>
+                <div className='splash-assets-content'>
+                    <div className='etfs-img'></div>
+                    <h1>ETFs</h1>
+                    <p>Diversify your holdings by buying into a bundle of stocks in a single investment.</p>
+                    <p className='splash-links'>Learn about ETFs</p>
+                </div>
+                <div className='splash-assets-content'>
+                    <div className='options-img'></div>
+                    <h1>Options</h1>
+                    <p>Choose to go long on stocks you believe in and short the ones you don’t.</p>
+                    <p className='splash-links'>Learn about options</p>
+                </div>
+                <div className='splash-assets-content'>
+                    <div className='crypto-img'></div>
+                    <h1>Crypto</h1>
+                    <p>Tap into the cryptocurrency market to trade <strong>Bitcoin</strong>, <strong>Ethereum</strong>, and <strong>more</strong>, 24/7.</p>
+                    <p className='splash-links'>Learn about crypto</p>
+                </div>
+            </div>
+            <div className='no-manual-needed'>
+                <div className='white-phone'></div>
+                <div className='white-phone-text'>
+                    <h1>No Manual Needed</h1>
+                    <p>
+                        Intuitively designed for newcomers and experts alike, Robinhood gives you a clear picture of your portfolio’s performance over time,
+                        so you can adjust your positions and learn by doing.
+                    </p>
+                </div>
+
+            </div>
 
         </div>
     )
