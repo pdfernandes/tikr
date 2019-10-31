108fcbafb50a5be4783d6b7f25a06f3a76c619c0
pfernandes <phelipe.fern@gmail.com>
2019-10-31 14:32:29 -0400

    include watchlist items

--- frontend/components/watchlist/watchlist_item.jsx
+++ frontend/components/watchlist/watchlist_item.jsx
@@ -1,36 +1,77 @@
 import React from "react";
-import * as StocksAPIUtil from '../../util/stocks_api_util';
-import { throws } from "assert";
-
+import * as StocksAPIUtil from "../../util/stocks_api_util";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
+import CustomTooltip from "../dashboard/tooltip_content";
+import { Link } from "react-router-dom";
 
 class WatchlistItem extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            test: 1
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      price: 0
+    };
+  }
 
-    componentDidMount() {
-        debugger
-        let { ticker } = this.props;
-        Promise.all([
-          StocksAPIUtil.getIntradayPrices(ticker),
-          StocksAPIUtil.getLastPrice(ticker)
-        ]).then(response => {
-            this.setState({
-                test : 2
-            })
-        });
-    }
+  componentDidMount() {
+    debugger;
+    let { ticker } = this.props;
+    Promise.all([
+      StocksAPIUtil.getIntradayPrices(ticker),
+      StocksAPIUtil.getLastPrice(ticker)
+    ]).then(response => {
+      debugger;
+      let dayPrices = response[0];
+      let lastPrice = response[1].last_price;
+      this.setState({
+        data: dayPrices.slice(0.93 * dayPrices.length),
+        price: lastPrice
+      });
+    });
+  }
 
-    render () {
-        debugger
-        return (
-            <div>{this.state.test}</div>
-        )
-    }
-
+  render() {
+    debugger;
+    return (
+      <div>
+        {/* <h1>{this.props.ticker}</h1> */}
+        <Link className="watchlist-link" to={`/stocks/${this.props.ticker}`}>
+          <div>
+            <h1>{this.props.ticker}</h1>
+        <h2>{`$ ${this.state.price.toFixed(2)}`}</h2>
+            {/* <h2>{portfolio[company]} shares</h2> */}
+          </div>
+          {/* <div className="portfolio-value">$ {totalValue}</div> */}
+          <div className="watchlist-chart">
+            <LineChart width={50} height={50} data={this.state.data}>
+              <XAxis
+                dataKey="minute"
+                hide={true}
+                domain={["dataMin", "dataMax"]}
+              />
+              <YAxis hide={true} domain={["dataMin", "dataMax"]} />
+              <Line
+                type="monotone"
+                connectNulls
+                dataKey="close"
+                stroke="#34D199"
+                strokeWidth="2"
+                dot={false}
+              />
+            </LineChart>
+          </div>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default WatchlistItem;
